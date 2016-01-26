9547958b59238de09193d8818dd3bbebd8647a81
Andrew Trigg <andrewt51@hotmail.com>
2016-01-26 22:51:00 +0000

    Works well with curveText.js for 'text above' with positive curvature

--- main.js
+++ main.js
@@ -3,7 +3,28 @@
 let curveApp = angular.module('curveApp',[])
 
 .controller('mainCtrl',function($scope){
+  let container = document.getElementById('displayArea')
   $scope.twoSpaces = "\u00A0\u00A0";
+  
+  $scope.applyCurve = function(){
+    container.innerHTML = '';
+    let element = curveText({ 
+      text: $scope.textToCurve,
+      textColor: $scope.textColor,
+      fontSize: $scope.fontSize,
+      backColor: $scope.backColor,
+      circleSize: $scope.circleSize,
+      under: $scope.under,
+      curvature: $scope.curvature,
+      lineSides: $scope.lineSides,
+      lineOnTop: $scope.lineOnTop,
+      lineOnBottom: $scope.lineOnBottom,
+      lengthOfLines:$scope.lengthOfLines,
+      lineColor: $scope.lineColor
+    })
+    container.appendChild(element)
+
+  }
   
 })
 
